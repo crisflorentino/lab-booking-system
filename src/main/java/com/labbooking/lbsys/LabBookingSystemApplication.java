9ce52762362b15488b155c670b3abc08baa4a900
crisflorentino <crisflorentino12@gmail.com>
2026-04-09 17:29:51 -0400

    chore: database setup

--- src/main/java/com/labbooking/lbsys/LabBookingSystemApplication.java
+++ src/main/java/com/labbooking/lbsys/LabBookingSystemApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-// Disabling the automatic configuration for DataSource since our SQL Database isn't set up yet.
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class LabBookingSystemApplication {
 
     public static void main(String[] args) {
