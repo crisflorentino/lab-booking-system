b386eb294cccbeb9e7746db969ebd213e9f44f31
crisflorentino <crisflorentino12@gmail.com>
2026-04-09 21:01:51 -0400

    feat: usercontroller, added temporary method that returns all users on GET request

---
+++ src/main/java/com/labbooking/lbsys/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.labbooking.lbsys.controller;
+
+import com.labbooking.lbsys.model.User;
+import com.labbooking.lbsys.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    // This is temporary, later we will move this to a service class.
+    private final UserRepository uRepo;
+
+    public UserController(UserRepository uRepo) {
+        this.uRepo = uRepo;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return uRepo.findAll();
+    }
+}
