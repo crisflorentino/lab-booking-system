dfecbe0b9efa2a723074098e9a4643a69aae9f8f
crisflorentino <crisflorentino12@gmail.com>
2026-04-27 14:29:01 -0400

    feat: equipmentservice

---
+++ src/main/java/com/labbooking/lbsys/service/EquipmentService.java
@@ -0,0 +1,65 @@
+package com.labbooking.lbsys.service;
+
+
+import com.labbooking.lbsys.model.Equipment;
+import com.labbooking.lbsys.model.Status;
+import com.labbooking.lbsys.repository.EquipmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EquipmentService {
+    private final EquipmentRepository eRepo;
+
+    public EquipmentService(EquipmentRepository eRepo) {
+        this.eRepo = eRepo;
+    }
+
+    public Equipment addEquipment(Equipment newEquipment) {
+
+        // If no status is provided, make the status automatically available.
+        if (newEquipment.getStatus() == null) {
+            newEquipment.setStatus(Status.AVAILABLE);
+        }
+
+        return eRepo.save(newEquipment);
+    }
+
+    public Equipment updateEquipmentStatus(Long id, Status newStatus) {
+        Optional<Equipment> existingEquipment = eRepo.findById(id);
+
+        if (existingEquipment.isEmpty()) {
+            throw new RuntimeException("Equipment update failed: ID " + id + " does not exist.");
+        }
+
+        Equipment equipment = existingEquipment.get();
+        equipment.setStatus(newStatus);
+
+        return eRepo.save(equipment);
+    }
+
+    public Equipment getEquipmentById(Long id) {
+        Optional<Equipment> existingEquipment = eRepo.findById(id);
+
+        if (existingEquipment.isEmpty()) {
+            throw new RuntimeException("Equipment update failed: ID " + id + " does not exist.");
+        }
+
+        return existingEquipment.get();
+    }
+
+    public List<Equipment> getAllEquipment() {
+        return eRepo.findAll();
+    }
+
+    public void deleteEquipment(Long id) {
+        // TODO: Check if equipment is linked to an active booking, do not allow deletion if this is the case.
+        if (!eRepo.existsById(id)) {
+            throw new RuntimeException("Equipment deletion failed: ID " + id + " does not exist.");
+        }
+
+        eRepo.deleteById(id);
+    }
+}
