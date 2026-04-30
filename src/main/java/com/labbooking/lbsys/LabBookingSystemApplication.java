85dc0bc85c1b695a6cb1912fa64d4268cf823fbd
crisflorentino <crisflorentino12@gmail.com>
2026-04-29 22:06:15 -0400

    chore: import stuff i forgot

--- src/main/java/com/labbooking/lbsys/LabBookingSystemApplication.java
+++ src/main/java/com/labbooking/lbsys/LabBookingSystemApplication.java
@@ -3,6 +3,10 @@ package com.labbooking.lbsys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LabBookingSystemApplication {
