48158384075c71d31fe771f712d824d204fb26b1
crisflorentino <crisflorentino12@gmail.com>
2026-04-29 21:24:44 -0400

    chore: equipment import

--- src/main/java/com/labbooking/lbsys/repository/BookingRepository.java
+++ src/main/java/com/labbooking/lbsys/repository/BookingRepository.java
@@ -1,6 +1,7 @@
 package com.labbooking.lbsys.repository;
 
 import com.labbooking.lbsys.model.Booking;
+import com.labbooking.lbsys.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
