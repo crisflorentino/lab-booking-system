e9eb0f0cc0f99dfd69d0d5a9dd6af762f4cbe0bf
crisflorentino <crisflorentino12@gmail.com>
2026-04-29 21:24:53 -0400

    chore: query and param imports

--- src/main/java/com/labbooking/lbsys/repository/BookingRepository.java
+++ src/main/java/com/labbooking/lbsys/repository/BookingRepository.java
@@ -3,6 +3,8 @@ package com.labbooking.lbsys.repository;
 import com.labbooking.lbsys.model.Booking;
 import com.labbooking.lbsys.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
