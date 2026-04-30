bf8228795490ebee89867928b00166b78a56b9fb
crisflorentino <crisflorentino12@gmail.com>
2026-04-29 21:24:29 -0400

    feat: postgresql query that checks if booking times overlap with others.

--- src/main/java/com/labbooking/lbsys/repository/BookingRepository.java
+++ src/main/java/com/labbooking/lbsys/repository/BookingRepository.java
@@ -9,4 +9,12 @@ import java.util.List;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByUserId(Long userId);
+
+    // PostgreSQL query that checks if there are any existing bookings for a piece of equipment that overlaps with current start and end times.
+    @Query("SELECT COUNT(b) > 0 FROM Booking b WHERE b.equipment = :equipment " + "AND b.startTime < :endTime AND b.endTime > :startTime")
+    boolean hasTimeConflict(
+            @Param("equipment") Equipment equipment,
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime
+    );
 }
